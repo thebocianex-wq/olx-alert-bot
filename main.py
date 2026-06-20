d26b7434e8621374d2ba9bccd54343fa868055ae
thebocianex-wq <thebocianex@gmail.com>
2026-06-20 23:12:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
+import os
 
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+requests.get(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    params={
+        "chat_id": CHAT_ID,
+        "text": "🚀 GitHub działa!"
+    }
+)
+
+print("OK")
